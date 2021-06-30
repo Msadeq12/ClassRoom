6d5e4507699e4718b435ff128e77bc4127eb4f2b
Sadeq <msadeq27@yahoo.com>
2021-06-30 22:04:00 +0600

    ADS

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const Mongo = require("mongoose");
+const crypto = require("crypto");
+const bodyParser = require("body-parser");
+const { urlencoded } = require("body-parser");
+
+
+const Website = express();
+Website.use(bodyParser.urlencoded({extended: false}));
+Website.use(express.static("/tools"));
+
+Website.set("views", "pages");
+Website.set("view engine", "ejs");
+
+Mongo.connect("mongodb://localhost:27017/ClassRoom",
+    {useNewUrlParser: true},
+    () => console.log("connected to ClassRoom DB...")
+);
+
+Website.get("/", (req,res) => {
+    res.render("sign-up");
+});
+
+Website.listen(1550, () => {
+    console.log("Listening at 1550...");
+})
