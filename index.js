c609a0733b6cc0aeaeb038e9ad277ec98f10f5d3
Sadeq <msadeq27@yahoo.com>
2021-07-27 22:22:11 +0600

    nodemailer + email

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ const bodyParser = require("body-parser");
 const { urlencoded } = require("body-parser");
 const {check, validationResult} = require("express-validator");
 const cookieParser = require('cookie-parser');
-const session = require("express-session");
-const internal = require("stream");
+const nodemailer = require('nodemailer');
 
 
 const Website = express();
@@ -244,6 +243,37 @@ Website.post('/signup', [
                 newUser.save().then(() => {
                     console.log('new user created');
                     res.cookie("SESSION_ID", sessionid, {httpOnly:true});
+
+                    // var transporter = nodemailer.createTransport({
+                    //     host: "smtp.gmail.com",
+                    //     port: "465",
+                    //     auth:
+                    //     {
+                    //         user: "",
+                    //         pass: ""
+                    //     }
+                    // });
+
+                    // var mailOptions = {
+                    //     from: "mohammadsadeq1214@gmail.com",
+                    //     to: email,
+                    //     subject: "Confirmation Email !",
+                    //     text: "You have created an account with us. Congratulations !"
+                    // }
+
+                    // transporter.sendMail(mailOptions, (err, info) => {
+                    //     if (err)
+                    //     {
+                    //         console.log(err);
+                    //     }
+
+                    //     else
+                    //     {
+                    //         console.log("Email sent: " + info.response);
+
+                    //     }
+                    // });
+
                     res.redirect("/");
                 });
             }
